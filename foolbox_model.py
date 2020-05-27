dab3852dbe1e05be882f9753c03182a68b09e3b2
maurapintor <maura.pintor@gmail.com>
2020-05-27 10:05:50 +0200

    release: v1.1

--- foolbox_model.py
+++ foolbox_model.py
@@ -7,10 +7,9 @@ from model import MNISTModel
 def create():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     weights_path = fb.zoo.fetch_weights(
-        'https://github.com/Maupin1991/mnist-pretrained/releases/download/v1.0/mnist_cnn.pt',
+        'https://github.com/Maupin1991/mnist-pretrained/releases/download/v1.1/mnist_cnn.pt',
         unzip=False
     )
-
     model = MNISTModel()
     state_dict = torch.load(weights_path, map_location=device)
     model.load_state_dict(state_dict)
@@ -24,8 +23,3 @@ def create():
                                     device=device)
 
     return fmodel
-
-m = create()
-samples, labels  = fb.utils.samples(m, dataset='MNIST', batchsize=10)
-print(labels)
-print(fb.utils.accuracy(m, samples, labels))
