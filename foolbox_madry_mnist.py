28163ea99d1440b8b39a872efdab9a170fddde06
maurapintor <maura.pintor@gmail.com>
2021-01-25 22:48:16 +0100

    fix: fixed loading models problem

--- foolbox_madry_mnist.py
+++ foolbox_madry_mnist.py
@@ -4,22 +4,21 @@ Model trained with https://github.com/louis2889184/pytorch-adversarial-training
 import foolbox as fb
 import torch
 
-from model import MNISTModel
+from model_madry import Model
 
 
 def create():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     weights_path = fb.zoo.fetch_weights(
-        'https://github.com/maurapintor/mnist-pretrained/releases/download/v1.2/mnist_challenge.pt',
+        'https://github.com/maurapintor/mnist-pretrained/releases/download/v1.2/madry_mnist.pt',
         unzip=False
     )
-    model = MNISTModel()
+    model = Model(i_c=1, n_c=10)
     state_dict = torch.load(weights_path, map_location=device)
     model.load_state_dict(state_dict)
     model.eval()
 
-    preprocessing = {'mean': 0.5,
-                     'std': 0.5}
+    preprocessing = None
 
     fmodel = fb.models.PyTorchModel(model, bounds=(0, 1),
                                     preprocessing=preprocessing,
--- foolbox_model.py
+++ foolbox_model.py
@@ -1,6 +1,7 @@
 import foolbox as fb
 import torch
 
+from model import MNISTModel
 from model_madry import Model
 
 
@@ -10,12 +11,13 @@ def create():
         'https://github.com/maurapintor/mnist-pretrained/releases/download/v1.2/mnist_cnn.pt',
         unzip=False
     )
-    model = Model(i_c=1, n_c=10)
+    model = MNISTModel()
     state_dict = torch.load(weights_path, map_location=device)
     model.load_state_dict(state_dict)
     model.eval()
 
-    preprocessing = None
+    preprocessing = {'mean': 0.5,
+                     'std': 0.5}
 
     fmodel = fb.models.PyTorchModel(model, bounds=(0, 1),
                                     preprocessing=preprocessing,
